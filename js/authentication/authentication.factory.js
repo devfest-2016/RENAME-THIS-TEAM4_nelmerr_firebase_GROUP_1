299121fe2490c37a5f7f1a5a187f633faa5d0ce4
aprietof <adrianprieto00@gmail.com>
2016-11-19 14:23:09 -0500

    auth database functionality added and rerouting

--- js/authentication/authentication.factory.js
+++ js/authentication/authentication.factory.js
@@ -1,18 +1,51 @@
-(function () {
+(function() {
 
-  'use strict'
+    'use strict'
 
-  function AuthenticationFactory() {
+    function AuthenticationFactory($state, $firebaseAuth) {
 
-    return {
+        // Firebase Authorization Object
+        var auth = firebase.auth()
+
+        return {
+            authObj: authObj,
+            user: user,
+            logout: logout
+        }
+
+        // ########## DEFINED FUNCTIONS ##########
+
+        // RETURN FIREBASE AUTH OBJECT
+        function authObj() {
+            return auth
+        }
+
+        // RETURN CURRENT USER OBJECT IF THERE IS ONE
+        function user() {
+            return auth.currentUser
+        }
+
+        // LOGOUT
+        function logout() {
+            auth.signOut().then(function() {
+                // Refresh Page
+                $state.go('home', {}, {
+                    reload: true
+                });
+                // Sign-out successful.
+                console.log('USER LOGGED OUT');
+
+            }, function(error) {
+                // An error happened.
+                console.error(error);
+            });
+        }
 
     }
 
-  }
 
-
-  angular
-    .module('app')
-    .factory('AuthenticationFactory', AuthenticationFactory)
+    angular
+        .module('app')
+        .factory('AuthenticationFactory', AuthenticationFactory)
 
 }())
--- js/routes.js
+++ js/routes.js
@@ -11,6 +11,21 @@
             templateUrl: 'js/home/home.html',
             controller: 'HomeController as vm'
           })
+          .state('registration', {
+            url: '/registration',
+            templateUrl: 'js/authentication/registration.html',
+            controller: 'AuthenticationController as vm'
+          })
+          .state('login', {
+            url: '/login',
+            templateUrl: 'js/authentication/login.html',
+            controller: 'AuthenticationController as vm'
+          })
+          .state('dashboard', {
+            url: '/dashboard',
+            templateUrl: 'js/dashboard/dashboard.html',
+            controller: 'DashboardController as vm'
+          })
         $urlRouterProvider.otherwise('/');
     });
 
