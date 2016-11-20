81258b989ee01e745bc701e0f4ae07d3b5b541d3
aprietof <adrianprieto00@gmail.com>
2016-11-20 08:13:43 -0500

    search vendors and favorites functionality added

--- js/dashboard/dashboard.controller.js
+++ js/dashboard/dashboard.controller.js
@@ -7,18 +7,23 @@
 
     const usersRef = firebase.database().ref('users')
     const productsRef = usersRef.child(user.$id).child('products')
+    const vendorsRef = usersRef.child(user.$id).child('vendors')
     const productsArray = $firebaseArray(productsRef)
+    const usersArray = $firebaseArray(usersRef)
+    const vendorsArray = $firebaseArray(vendorsRef)
 
     // vm VARIABLES
     vm.store = user
     vm.products = productsArray
+    vm.users = usersArray
+    vm.myVendors = vendorsArray
 
 
     // CALLABLE METHODS
     vm.addProduct = addProduct
     vm.deleteProduct = deleteProduct
     vm.editProduct = editProduct
-
+    vm.addVendor = addVendor
 
     // INSTANTIADED METHODS
 
@@ -57,6 +62,36 @@
     function editProduct() {
       console.log('editing...');
     }
+
+
+    // ADD VENDOR TO MY VENDORS
+    function addVendor(vendor) {
+      for (var i = 0; i < vendorsArray.length; i++) {
+        if (vendorsArray[i].uid === vendor.uid) {
+          var toastContent = '<i style="color: yellow;" class="fa fa-exclamation-triangle fa-lg" aria-hidden="true"></i> This user is already a favorite'
+          // Display success message
+          Materialize.toast(toastContent, 3000)
+          return
+        }
+      }
+      vendorsArray.$add(vendor)
+      .then(function (data) {
+        var toastContent = '<i style="color: green;" class="fa fa-check-circle fa-lg" aria-hidden="true"></i> Vendor Added to Favorites'
+        // Display success message
+        Materialize.toast(toastContent, 3000)
+      })
+      .catch(function (error) {
+        console.error(error);
+      })
+    }
+        //
+        //
+        // usersRef.child(userData.uid).set({
+        //     uid: userData.uid,
+        //     firstname: user.name,
+        //     email: userData.email,
+        //     role: user.role
+        // })
 
 
   }
